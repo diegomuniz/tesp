503fc1ece487775d9a06c6d54290f7d0cbb76c12
Diego Muniz <yabazaki@gmail.com>
2015-08-31 22:36:35 -0300

    Insert and find

--- teste01/src/br/unibh/persistencia/AlunoDAO.java
+++ teste01/src/br/unibh/persistencia/AlunoDAO.java
@@ -1,71 +1,103 @@
 package br.unibh.persistencia;
 
-import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import br.unibh.entidades.Aluno;
 
-public class AlunoDAO implements DAO<Aluno,Long> {
-	
-	private static Connection con = null;
+public class AlunoDAO implements DAO<Aluno, Long> {
+
+	public static SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 
 	@Override
 	public Aluno find(Long id) {
-		// TODO Auto-generated method stub
+		try {
+			PreparedStatement p = JDBCUtil.getConnection().prepareStatement("select * from tb_aluno where id = ?");
+			p.setLong(1, id);
+			ResultSet res = p.executeQuery();
+
+			if (res.next()) {
+				return new Aluno(res.getLong("id"), res.getString("nome"), res.getString("cpf"),
+						res.getString("matricula"), res.getDate("data_aniversario"));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.closeConnection();
+		}
+		return null;
+	}
+
+	public Aluno find(String nome) {
+		try {
+			PreparedStatement p = JDBCUtil.getConnection().prepareStatement("select * from tb_aluno where nome like %");
+			p.setString(1, nome);
+			ResultSet res = p.executeQuery();
+
+			if (res.next()) {
+				return new Aluno(res.getLong("id"), res.getString("nome"), res.getString("cpf"),
+						res.getString("matricula"), res.getDate("data_aniversario"));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.closeConnection();
+		}
 		return null;
 	}
 
 	@Override
 	public void insert(Aluno t) {
 		// TODO Auto-generated method stub
+		try {
+			PreparedStatement p = JDBCUtil.getConnection().prepareStatement(
+					"insert into tb_aluno (nome, cpf, matricula, data_aniversario values (?,?,?,?)");
+			p.setString(1, t.getNome());
+			p.setString(2, t.getCpf());
+			p.setString(3, t.getMatricula());
+			p.setString(4, df.format(t.getDataAniversario()));
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.closeConnection();
+		}
 		
 	}
 
 	@Override
 	public void update(Aluno t) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void delete(Aluno t) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public List<Aluno> findAll() {
 		ArrayList<Aluno> lista = new ArrayList<Aluno>();
 		try {
-			con = JDBCUtil.getConnection();
-			ResultSet res = con.prepareStatement(
-					"select * from tb_aluno").executeQuery();
-			while (res.next()){
-				lista.add(
-						new Aluno(
-								res.getLong("id"), 
-								res.getString("nome"), 
-								res.getString("cpf"), 
-								res.getString("matricula"), 
-						null)
-						);
+			ResultSet res = JDBCUtil.getConnection().prepareStatement("select * from tb_aluno").executeQuery();
+			while (res.next()) {
+				lista.add(new Aluno(res.getLong("id"), res.getString("nome"), res.getString("cpf"),
+						res.getString("matricula"), res.getDate("data_aniversario")));
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			try {
-				JDBCUtil.closeConnection();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			JDBCUtil.closeConnection();
 		}
 		return lista;
 	}
-	
-	
 
 }
--- teste01/src/br/unibh/teste01/Main.java
+++ teste01/src/br/unibh/teste01/Main.java
@@ -1,20 +1,9 @@
 package br.unibh.teste01;
 
-import java.util.List;
-
-import br.unibh.entidades.Aluno;
-import br.unibh.persistencia.AlunoDAO;
 
 public class Main {
 
-	public static void main(String[] args) {
-		AlunoDAO dao = new AlunoDAO();
-		List<Aluno> lista = dao.findAll();
-		for (Aluno a: lista) {
-			System.out.println(a);
-		}
-		
-	}
+	
 	
 	
 }
