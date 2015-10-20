6faea920487e6a4c0a7dc9bbe8aaaa9ce96d4350
Diego Muniz <yabazaki@gmail.com>
2015-10-19 22:01:45 -0200

    Inicio do exercicio 5 - Aluno

--- escola/src/main/java/br/unibh/escola/entidades/Aluno.java
+++ escola/src/main/java/br/unibh/escola/entidades/Aluno.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -25,7 +27,10 @@ import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
 @PrimaryKeyJoinColumn
-@Table(name="TB_ALUNO", uniqueConstraints = @UniqueConstraint(columnNames = "matricula") )
+@Table(name = "TB_ALUNO", uniqueConstraints = @UniqueConstraint(columnNames = "matricula"))
+@NamedQueries({
+@NamedQuery(name="Aluno.findByName", query = "select a from Aluno a where a.nome like :nome")
+})
 public class Aluno extends Pessoa {
 
 	@Column(length=8, nullable=false, columnDefinition="CHAR(11)")
